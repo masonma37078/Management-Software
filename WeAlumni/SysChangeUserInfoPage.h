872cd59ce83a4e0a8ae66615b10df71fbae17ec4
changpian8 <56983240+changpian8@users.noreply.github.com>
2020-04-08 22:17:10 -0700

    Update SysChangeUserInfoPage files and SysInfoPage (#30)
    * added UI and revised StfInfoPage and StfAddPage files
    
    * revised StfInfoPage.cpp and StfAddPage.cpp
    
    * add SysChangeUserInfoPage files and SysInfoPage files

---
+++ WeAlumni/SysChangeUserInfoPage.h
@@ -0,0 +1,206 @@
+#pragma once
+#include "Database.h"
+
+/*
+ * SysChangeUserInfoPage.h
+ *
+ * This file have basic username/password interaction actions.
+ *
+ * @author: Rui Jia
+ * Revised: 4/7/20
+ *
+ */
+
+namespace WeAlumni {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for SysChangeUserInfoPage
+	/// </summary>
+	public ref class SysChangeUserInfoPage : public System::Windows::Forms::Form
+	{
+	public:
+		SysChangeUserInfoPage(String^ UserName)
+		{
+			InitializeComponent();
+			_UserName = UserName;
+			Initialize();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~SysChangeUserInfoPage()
+		{
+			if (components)
+			{
+				delete components;
+			}
+			if (_database)
+			{
+				delete _database;
+			}
+		}
+	private: System::Windows::Forms::Label^ lbl_prompt_changeUser;
+	private: System::Windows::Forms::Label^ lbl_prompt_changePass;
+	private: System::Windows::Forms::TextBox^ txt_changeUser;
+	private: System::Windows::Forms::TextBox^ txt_changePass;
+	private: System::Windows::Forms::Button^ btn_Confirm;
+	private: System::Windows::Forms::Button^ btn_Cancle;
+	private: System::Windows::Forms::Label^ lbl_Error;
+	private: System::Windows::Forms::Label^ label1;
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container^ components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->lbl_prompt_changeUser = (gcnew System::Windows::Forms::Label());
+			this->lbl_prompt_changePass = (gcnew System::Windows::Forms::Label());
+			this->txt_changeUser = (gcnew System::Windows::Forms::TextBox());
+			this->txt_changePass = (gcnew System::Windows::Forms::TextBox());
+			this->btn_Confirm = (gcnew System::Windows::Forms::Button());
+			this->btn_Cancle = (gcnew System::Windows::Forms::Button());
+			this->lbl_Error = (gcnew System::Windows::Forms::Label());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// lbl_prompt_changeUser
+			// 
+			this->lbl_prompt_changeUser->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->lbl_prompt_changeUser->AutoSize = true;
+			this->lbl_prompt_changeUser->Font = (gcnew System::Drawing::Font(L"SimSun", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(134)));
+			this->lbl_prompt_changeUser->Location = System::Drawing::Point(29, 94);
+			this->lbl_prompt_changeUser->Name = L"lbl_prompt_changeUser";
+			this->lbl_prompt_changeUser->Size = System::Drawing::Size(185, 19);
+			this->lbl_prompt_changeUser->TabIndex = 0;
+			this->lbl_prompt_changeUser->Text = L"Change User Name";
+			// 
+			// lbl_prompt_changePass
+			// 
+			this->lbl_prompt_changePass->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->lbl_prompt_changePass->AutoSize = true;
+			this->lbl_prompt_changePass->Font = (gcnew System::Drawing::Font(L"SimSun", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(134)));
+			this->lbl_prompt_changePass->Location = System::Drawing::Point(29, 163);
+			this->lbl_prompt_changePass->Name = L"lbl_prompt_changePass";
+			this->lbl_prompt_changePass->Size = System::Drawing::Size(174, 19);
+			this->lbl_prompt_changePass->TabIndex = 1;
+			this->lbl_prompt_changePass->Text = L"Change Password";
+			// 
+			// txt_changeUser
+			// 
+			this->txt_changeUser->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->txt_changeUser->Location = System::Drawing::Point(242, 93);
+			this->txt_changeUser->Name = L"txt_changeUser";
+			this->txt_changeUser->Size = System::Drawing::Size(100, 25);
+			this->txt_changeUser->TabIndex = 2;
+			// 
+			// txt_changePass
+			// 
+			this->txt_changePass->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->txt_changePass->Location = System::Drawing::Point(242, 162);
+			this->txt_changePass->Name = L"txt_changePass";
+			this->txt_changePass->Size = System::Drawing::Size(100, 25);
+			this->txt_changePass->TabIndex = 3;
+			// 
+			// btn_Confirm
+			// 
+			this->btn_Confirm->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->btn_Confirm->Font = (gcnew System::Drawing::Font(L"SimSun", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(134)));
+			this->btn_Confirm->Location = System::Drawing::Point(33, 267);
+			this->btn_Confirm->Name = L"btn_Confirm";
+			this->btn_Confirm->Size = System::Drawing::Size(223, 64);
+			this->btn_Confirm->TabIndex = 4;
+			this->btn_Confirm->Text = L"Confirm";
+			this->btn_Confirm->UseVisualStyleBackColor = true;
+			this->btn_Confirm->Click += gcnew System::EventHandler(this, &SysChangeUserInfoPage::btn_Confirm_Click);
+			// 
+			// btn_Cancle
+			// 
+			this->btn_Cancle->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->btn_Cancle->Font = (gcnew System::Drawing::Font(L"SimSun", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(134)));
+			this->btn_Cancle->Location = System::Drawing::Point(274, 267);
+			this->btn_Cancle->Name = L"btn_Cancle";
+			this->btn_Cancle->Size = System::Drawing::Size(223, 64);
+			this->btn_Cancle->TabIndex = 5;
+			this->btn_Cancle->Text = L"Cancle";
+			this->btn_Cancle->UseVisualStyleBackColor = true;
+			this->btn_Cancle->Click += gcnew System::EventHandler(this, &SysChangeUserInfoPage::btn_Cancle_Click);
+			// 
+			// lbl_Error
+			// 
+			this->lbl_Error->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->lbl_Error->AutoSize = true;
+			this->lbl_Error->Font = (gcnew System::Drawing::Font(L"SimSun", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(134)));
+			this->lbl_Error->ForeColor = System::Drawing::Color::Red;
+			this->lbl_Error->Location = System::Drawing::Point(238, 227);
+			this->lbl_Error->Name = L"lbl_Error";
+			this->lbl_Error->Size = System::Drawing::Size(64, 19);
+			this->lbl_Error->TabIndex = 6;
+			this->lbl_Error->Text = L"ERROR";
+			this->lbl_Error->Visible = false;
+			// 
+			// label1
+			// 
+			this->label1->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"SimSun", 15, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(134)));
+			this->label1->Location = System::Drawing::Point(157, 29);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(264, 25);
+			this->label1->TabIndex = 7;
+			this->label1->Text = L"Change Information";
+			// 
+			// SysChangeUserInfoPage
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 15);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(568, 417);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->lbl_Error);
+			this->Controls->Add(this->btn_Cancle);
+			this->Controls->Add(this->btn_Confirm);
+			this->Controls->Add(this->txt_changePass);
+			this->Controls->Add(this->txt_changeUser);
+			this->Controls->Add(this->lbl_prompt_changePass);
+			this->Controls->Add(this->lbl_prompt_changeUser);
+			this->Name = L"SysChangeUserInfoPage";
+			this->Text = L"SysChangeUserInfoPage";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+		}
+	private:
+		String^ _UserName;
+		Database^ _database;
+
+	private:
+		Void Initialize();
+		Void UpdateInfo();
+		Void btn_Confirm_Click(System::Object^ sender, System::EventArgs^ e);
+		Void btn_Cancle_Click(System::Object^ sender, System::EventArgs^ e);
+#pragma endregion
+	};
+}
---
+++ WeAlumni/SysInfoPage.h
@@ -0,0 +1,196 @@
+#pragma once
+
+/*
+ * SysInfoPage.h
+ *
+ * This file have basic information of system.
+ *
+ * @author: Rui Jia
+ * Revised: 4/7/20
+ *
+ */
+
+namespace WeAlumni {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for SysInfoPage
+	/// </summary>
+	public ref class SysInfoPage : public System::Windows::Forms::Form
+	{
+	public:
+		SysInfoPage(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~SysInfoPage()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ lbl_Prompt_Version;
+	private: System::Windows::Forms::Label^ lbl_Version;
+	private: System::Windows::Forms::Label^ lbl_Prompt_Intro;
+	private: System::Windows::Forms::Label^ lbl_Prompt_Feed;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::Label^ lbl_Prompt_Con;
+	private: System::Windows::Forms::Label^ lbl_Con;
+	private: System::Windows::Forms::Label^ label1;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container^ components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->lbl_Prompt_Version = (gcnew System::Windows::Forms::Label());
+			this->lbl_Version = (gcnew System::Windows::Forms::Label());
+			this->lbl_Prompt_Intro = (gcnew System::Windows::Forms::Label());
+			this->lbl_Prompt_Feed = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->lbl_Prompt_Con = (gcnew System::Windows::Forms::Label());
+			this->lbl_Con = (gcnew System::Windows::Forms::Label());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// lbl_Prompt_Version
+			// 
+			this->lbl_Prompt_Version->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->lbl_Prompt_Version->AutoSize = true;
+			this->lbl_Prompt_Version->Font = (gcnew System::Drawing::Font(L"SimSun", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(134)));
+			this->lbl_Prompt_Version->Location = System::Drawing::Point(26, 81);
+			this->lbl_Prompt_Version->Name = L"lbl_Prompt_Version";
+			this->lbl_Prompt_Version->Size = System::Drawing::Size(218, 19);
+			this->lbl_Prompt_Version->TabIndex = 0;
+			this->lbl_Prompt_Version->Text = L"Version Information";
+			// 
+			// lbl_Version
+			// 
+			this->lbl_Version->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->lbl_Version->AutoSize = true;
+			this->lbl_Version->Font = (gcnew System::Drawing::Font(L"SimSun", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(134)));
+			this->lbl_Version->Location = System::Drawing::Point(279, 81);
+			this->lbl_Version->Name = L"lbl_Version";
+			this->lbl_Version->Size = System::Drawing::Size(119, 19);
+			this->lbl_Version->TabIndex = 1;
+			this->lbl_Version->Text = L"Version 1.0";
+			// 
+			// lbl_Prompt_Intro
+			// 
+			this->lbl_Prompt_Intro->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->lbl_Prompt_Intro->AutoSize = true;
+			this->lbl_Prompt_Intro->Font = (gcnew System::Drawing::Font(L"SimSun", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(134)));
+			this->lbl_Prompt_Intro->Location = System::Drawing::Point(103, 143);
+			this->lbl_Prompt_Intro->Name = L"lbl_Prompt_Intro";
+			this->lbl_Prompt_Intro->Size = System::Drawing::Size(141, 19);
+			this->lbl_Prompt_Intro->TabIndex = 2;
+			this->lbl_Prompt_Intro->Text = L"Introduction";
+			// 
+			// lbl_Prompt_Feed
+			// 
+			this->lbl_Prompt_Feed->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->lbl_Prompt_Feed->AutoSize = true;
+			this->lbl_Prompt_Feed->Font = (gcnew System::Drawing::Font(L"SimSun", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(134)));
+			this->lbl_Prompt_Feed->Location = System::Drawing::Point(147, 209);
+			this->lbl_Prompt_Feed->Name = L"lbl_Prompt_Feed";
+			this->lbl_Prompt_Feed->Size = System::Drawing::Size(97, 19);
+			this->lbl_Prompt_Feed->TabIndex = 4;
+			this->lbl_Prompt_Feed->Text = L"Feedback";
+			// 
+			// label6
+			// 
+			this->label6->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->label6->AutoSize = true;
+			this->label6->Font = (gcnew System::Drawing::Font(L"SimSun", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(134)));
+			this->label6->Location = System::Drawing::Point(279, 143);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(219, 19);
+			this->label6->TabIndex = 5;
+			this->label6->Text = L"Contact: caa@ucsc.edu";
+			// 
+			// lbl_Prompt_Con
+			// 
+			this->lbl_Prompt_Con->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->lbl_Prompt_Con->AutoSize = true;
+			this->lbl_Prompt_Con->Font = (gcnew System::Drawing::Font(L"SimSun", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(134)));
+			this->lbl_Prompt_Con->Location = System::Drawing::Point(92, 273);
+			this->lbl_Prompt_Con->Name = L"lbl_Prompt_Con";
+			this->lbl_Prompt_Con->Size = System::Drawing::Size(152, 19);
+			this->lbl_Prompt_Con->TabIndex = 6;
+			this->lbl_Prompt_Con->Text = L"Configuration";
+			// 
+			// lbl_Con
+			// 
+			this->lbl_Con->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->lbl_Con->AutoSize = true;
+			this->lbl_Con->Font = (gcnew System::Drawing::Font(L"SimSun", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(134)));
+			this->lbl_Con->Location = System::Drawing::Point(279, 273);
+			this->lbl_Con->Name = L"lbl_Con";
+			this->lbl_Con->Size = System::Drawing::Size(189, 19);
+			this->lbl_Con->TabIndex = 7;
+			this->lbl_Con->Text = L".NET Framework 4.0";
+			// 
+			// label1
+			// 
+			this->label1->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"SimSun", 15, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(134)));
+			this->label1->Location = System::Drawing::Point(134, 22);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(264, 25);
+			this->label1->TabIndex = 8;
+			this->label1->Text = L"System Information";
+			// 
+			// SysInfoPage
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 15);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(548, 359);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->lbl_Con);
+			this->Controls->Add(this->lbl_Prompt_Con);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->lbl_Prompt_Feed);
+			this->Controls->Add(this->lbl_Prompt_Intro);
+			this->Controls->Add(this->lbl_Version);
+			this->Controls->Add(this->lbl_Prompt_Version);
+			this->Name = L"SysInfoPage";
+			this->Text = L"SysInfoPage";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	};
+}
